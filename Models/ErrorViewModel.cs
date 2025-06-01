0546d4e787ac5e5124a1157a7ee07464f9fd21d8
Emre Okcelen <emreokcelen03@gmail.com>
2025-06-02 00:57:41 +0300

    Add initial website files

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace EmreOkcelenCV.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+using Microsoft.AspNetCore.Localization;
+using System.Globalization;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
+
+builder.Services.AddControllersWithViews()
+    .AddViewLocalization()
+    .AddDataAnnotationsLocalization();
+
+var app = builder.Build();
+
+// Supported cultures (diller)
+var supportedCultures = new[]
+{
+    new CultureInfo("en"),
+    new CultureInfo("tr")
+};
+
+// Localization middleware'i ekle
+app.UseRequestLocalization(new RequestLocalizationOptions
+{
+    DefaultRequestCulture = new RequestCulture("en"),
+    SupportedCultures = supportedCultures,
+    SupportedUICultures = supportedCultures
+});
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
